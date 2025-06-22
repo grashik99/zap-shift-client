34cd9789f8e752ebe0b3a96fdc04fd734dd56231
grashik99 <grashik99@gmail.com>
2025-06-22 20:37:42 +0300

    Basic layouts Emplemented

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { router } from "./router/router.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <div className="urbanist-font max-w-7xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>
 );
---
+++ src/pages/shared/ProFastLogo/ProFastLogo.jsx
@@ -0,0 +1,11 @@
+import logo from "../../../assets/logo.png"
+
+const ProFastLogo = () => {
+  return (
+    <div className="flex items-end">
+        <img src={logo} alt="logo" className="mb-2"/>
+        <p className="font-extrabold text-3xl -ms-2">Profast</p>
+    </div>
+  )
+}
+export default ProFastLogo
